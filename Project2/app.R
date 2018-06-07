613c7a947a4f6448e0f71814fb9e0f6da262d6df
fsommer95 <florian.sommer@st.ovgu.de>
2018-06-07 09:22:57 +0200

    update

--- Project2/app.R
+++ Project2/app.R
@@ -13,6 +13,9 @@ library(tidyr)
 library(stringi)
 library(cluster)
 library("factoextra")
+library(ggfortify)
+library(cluster)
+library(ggfortify)
 
 
 
@@ -54,10 +57,18 @@ ui <- dashboardPage(
         
         sliderInput("centroids", h3("Number of centroids"),
                     min = 2, max = 20, value = 2),
-        
-        plotOutput("clustering")
-        
-      ))
+        # k is the number of clusters
+        plotOutput("k")
+      ),
+      
+      box(
+        plotOutput("plot1"),
+        plotOutput("plot2")
+
+      )
+      
+      
+      )
     
 
     
@@ -105,54 +116,86 @@ server<-function(input,output){
   })
   
   popularity_rank_frame <- reactive({  
-    
     data.frame(prep_data_frame(), popularity_score()) %>% select(-c(5,6,7)) %>% setNames(c("year","sex","rank","forename","popularity_score")) 
     
   })
   
   
-
-    
-    
     popularity_rank_frame_filled <- reactive({  
       popularity_rank_frame() %>% complete(forename, year = 1890:2017,
         fill = list(popularity_score= 0))
     })
+    
       
-    popularity_rank_frame_filled_reshape <- reactive({  
-      
-
+    #reshape the popularity rank matrix
+   nice_data <- reactive({  
       x <- popularity_rank_frame_filled()
       #Remove the sex and rank collumn
       x$sex  <- NULL
       x$rank <- NULL
-      
       #reshape dataframe 
      ## x %>% gather (forename, year)
       x <- spread(x, key = year, value = popularity_score)
-
       return(x)
       })
     
+   
+   #reduce 128 Dimensions to 2 in a meaningful way --> use just 2 years --> prcomp()
+   # Default S3 method:
+   #prcomp(x, retx = TRUE, center = TRUE, scale. = FALSE,
+   #       tol = NULL, rank. = NULL, ...)
+      #just use the numbers and reduce the names
+      #nice_data.active <- nice_data[1:379, 1:129]
+    #first create a numeric matrix!!!
+   #test2 <- data.matrix(nice_data[1:379, 2:129])
+   #autoplot(prcomp(test2), data = nice_data)
+   
+   nice_data2 <- reactive({  
+     data.matrix(nice_data()[1:379, 2:129])
+   })
+   
+   #plot the data --> scatterplot
+   
 
-  
-  test <- reactive({ 
-    data.frame( popularity_rank_frame_filled_reshape())
+   
+   
+    metroids1 <- reactive({
+      autoplot(prcomp(nice_data2()), data = nice_data())
+          })
     
-  })
-  
+    metroids2 <- reactive({
+      clusplot(pam(nice_data2(), input$centroids), nice_data()[1] , labels = 4, lines = 0, color = TRUE, stand = TRUE)
+      #autoplot(prcomp(nice_data2()), data = nice_data(), label.size = input$k)
+      #autoplot(pam(nice_data2()[-5], 3), frame = TRUE, frame.type = 'norm')
+     
+      })
+    
 
+    test <- reactive({ 
+     data.frame(nice_data())
+    }) 
+
+  
+  ##################################################################################################################
   
   output$Prepared_Data <- renderDT(
     test(),
     options = list(lengthChange = FALSE),
     rownames = FALSE)
+    
+    
+    output$plot2  <- renderPlot({
+      metroids2()
+    })   
+  output$plot1  <- renderPlot({
+    metroids1()
+  }) 
+
   
 
 }
 
 #output$clustering <- fviz_cluster(clustering, popularity_rank_frame_filled_reshape())
-
 
 shinyApp(ui=ui, server=server)
 
